7fd4006ceb91bbda1bbe907b56dbff70a4d6e5f9
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-01-26 14:18:22 -0800

    Add request parameter to on_new_response callback
    That's our current way to tie request and response together.
    
    Also added typing information to callback functions.

--- src/pyproxy/callback.py
+++ src/pyproxy/callback.py
@@ -1,15 +1,53 @@
+from asyncio.streams import StreamReader
 from enum import IntEnum
+from httprequest import HttpRequest, HttpResponse
+from typing import Union
+
+"""
+
+----------
+| Server |
+----------
+    ^  |
+    |  |     -----------------
+    |  +---> |               |
+    +------- |               |
+             | Proxy Handler |
+    +------> |               |
+    |  +---- |               |
+    |  |     -----------------
+    |  v
+----------
+| Client |
+----------
+
+For new requests, the proxy handler options are:
+
+- Handler forwards request to server as-is (default) and awaits response
+- Handler intercepts request, modifies or replaces it and forwards to server
+- Handler suppresses request to server and returns response to client by itself
+
+For server response, the handler options are:
+
+- Handler forwards response to client as-is (default)
+- Handler intercepts response, modifies or replaces it and has it forwarded to client
+
+"""
 
 class ProxyServerAction(IntEnum):
     Forward = 1
-    Intercept = 2
 
 
 class ProxyServerCallback:
-    async def on_new_request_async(self, request) -> ProxyServerAction:
+    async def on_new_request_async(
+        self, request: HttpRequest) -> Union[ProxyServerAction, StreamReader]:
+
         print("ProxyServerCallback:on_new_request_async: returning ProxyServerAction.Forward")
         return ProxyServerAction.Forward
 
-    async def on_new_response_async(self, response) -> ProxyServerAction:
+    async def on_new_response_async(
+        self, request: HttpRequest, response: HttpResponse) -> Union[
+            ProxyServerAction, StreamReader]:
+
         print("ProxyServerCallback:on_new_response_async: returning ProxyServerAction.Forward")
         return ProxyServerAction.Forward
