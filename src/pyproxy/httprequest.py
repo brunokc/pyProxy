c6dc8296edf90ce5d127e1d7147243f03f5f7aac
Bruno Kraychete da Costa <brunokc@kraychete.com>
2023-01-26 17:18:24 -0800

    Refactored HttpRequest and HttpResponse
    Created a common base class containing the common code between the two.

--- src/pyproxy/httprequest.py
+++ src/pyproxy/httprequest.py
@@ -16,19 +16,11 @@ def parse_form_data(form_data):
     return values
 
 
-class HttpRequest:
-    def __init__(self, addr, reader: StreamReader):
+class HttpRequestResponseBase:
+    def __init__(self, reader: StreamReader):
         self._reader = reader
-        self.clientip, self.clientport = addr
-        self.raw_request = None
-        self.method = None
-        self.raw_url = None
-        self.version = None
-        self.url = None
         self.headers = { }
         self.body = b""
-        _LOGGER.debug("HttpRequest: clientip=%s, clientport=%d", self.clientip,
-            self.clientport)
 
     async def _read_headers(self):
         try:
@@ -37,37 +29,13 @@ class HttpRequest:
         except asyncio.IncompleteReadError as e:
             return e.partial
 
-    async def read_headers(self):
-        data = await self._read_headers()
-        _LOGGER.debug(f"HttpRequest: received %s", data)
-        if len(data) == 0:
-            return 0
-
-        self.raw_request = data
-        http_request_lines = data.split(b"\r\n")
-
-        # Split the request (first) line
-        request_line = http_request_lines[0].decode()
-        self.method, self.raw_url, self.version = request_line.split(" ")
-        self.url = urlparse(self.raw_url)
-
-        body_index = 1
-        self.headers = { }
-        for line in http_request_lines[1:]:
+    def _parse_headers(self, lines):
+        self.headers.clear()
+        for line in lines:
             if not line:
                 break
             name, value = line.decode().split(": ")
             self.headers[name] = value
-            body_index += 1
-
-        # self.body = b"".join(http_request_lines[body_index + 1:])
-
-        _LOGGER.debug("HttpRequest: %s", self)
-        return len(data)
-
-    def __str__(self):
-        return str(dict(url=self.url, hostname=self.url.hostname, port=self.url.port,
-            method=self.method))
 
     def get_streamreader(self):
         if self.body:
@@ -85,19 +53,55 @@ class HttpRequest:
         return self.body
 
 
-class HttpResponse:
+class HttpRequest(HttpRequestResponseBase):
+    def __init__(self, addr, reader: StreamReader):
+        super().__init__(reader)
+        self.clientip, self.clientport = addr
+        self.raw_request = None
+        self.method = None
+        self.raw_url = None
+        self.version = None
+        self.url = None
+        _LOGGER.debug("HttpRequest: clientip=%s, clientport=%d", self.clientip,
+            self.clientport)
+
+    async def read_headers(self):
+        data = await self._read_headers()
+        _LOGGER.debug(f"HttpRequest: received %s", data)
+        if len(data) == 0:
+            return 0
+
+        self.raw_request = data
+        http_request_lines = data.split(b"\r\n")
+
+        # Split the request (first) line
+        request_line = http_request_lines[0].decode()
+        self.method, self.raw_url, self.version = request_line.split(" ")
+        self.url = urlparse(self.raw_url)
+
+        self._parse_headers(http_request_lines[1:])
+
+        _LOGGER.debug("HttpRequest: %s", self)
+        return len(data)
+
+    def __str__(self):
+        return str(dict(url=self.url, hostname=self.url.hostname, port=self.url.port,
+            method=self.method))
+
+
+class HttpResponse(HttpRequestResponseBase):
     def __init__(self, reader: StreamReader):
-        self._reader = reader
+        super().__init__(reader)
         self.raw_response = None
         self.http_version = None
         self.response_code = None
         self.response_text = None
-        self.headers = { }
 
     async def read(self):
-        # Read the response line and all the response headers
-        data = await self._reader.readuntil(b"\r\n\r\n")
+        data = await self._read_headers()
         _LOGGER.debug("HttpResponse: received %s", data)
+        if len(data) == 0:
+            return 0
 
         self.raw_response = data
         http_response_lines = data.split(b"\r\n")
@@ -106,25 +110,8 @@ class HttpResponse:
         response_line = http_response_lines[0].decode()
         self.http_version, self.response_code, self.response_text = response_line.split(" ")
 
-        body_index = 1
-        self.headers = { }
-        for line in http_response_lines[1:]:
-            if not line:
-                break
-            name, value = line.decode().split(": ")
-            self.headers[name] = value
-            body_index += 1
+        self._parse_headers(http_response_lines[1:])
 
     def __str__(self):
         return str(dict(http_version=self.http_version,
             response_code=self.response_code, response_text=self.response_text))
-
-    def get_streamreader(self):
-        return self._reader
-
-    def get_response_length(self):
-        return int(self.headers["Content-Length"]
-            if "Content-Length" in self.headers else -1)
-
-    async def read_body(self):
-        return self._reader.read(self.get_response_length())
