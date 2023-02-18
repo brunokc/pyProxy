028009df243fe510bb914585436bbc46b5ef290e
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-02-17 22:30:22 -0800

    Cleanup of test code and more fixture usage
    Also:
    - Made a class around aiorequests to encapsulate proxy usage
    - Moved proxy server to a fixture so we won't have to cleanup on each test
    - Ensured pytest_asyncio mode is set to auto, so we won't have to use
      the asyncio mark on each test (@pytest.mark.asyncio)

--- tests/aiorequests.py
+++ tests/aiorequests.py
@@ -1,9 +1,14 @@
 import aiohttp
 
 
-async def get(url, *args, **kargs):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, *args, **kargs) as response:
-            # Ensure response is read before the connection is closed
-            await response.read()
-            return response
+class Requests:
+    def __init__(self, proxy_ip: str, proxy_port: int):
+        self._proxy_entry = { "proxy": f"http://{proxy_ip}:{proxy_port}" }
+
+    async def get(self, url, *args, **kargs):
+        async with aiohttp.ClientSession() as session:
+            kargs.update(self._proxy_entry)
+            async with session.get(url, *args, **kargs) as response:
+                # Ensure response is read before the connection is closed
+                await response.read()
+                return response
--- tests/test_simple_request.py
+++ tests/test_simple_request.py
@@ -7,45 +7,40 @@ from pytest_httpserver import HTTPServer
 import aiorequests
 from pyproxy.proxyserver import ProxyServer
 
-OWN_IP = "127.0.0.1"
-PROXY_IP = ""
+LOOPBACK = "127.0.0.1"
+HTTP_SERVER_PORT = 9998
 PROXY_PORT = 9999
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_logging() -> None:
-    logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(message)s")
 
 @pytest.fixture(scope="session")
 def httpserver_listen_address():
-    return (OWN_IP, 9998)
+    _LOGGER.debug(">>> httpserver_listen_address")
+    return (LOOPBACK, HTTP_SERVER_PORT)
+
+@pytest.fixture
+def aiorequest():
+    _LOGGER.debug(">>> aiorequest")
+    return aiorequests.Requests(LOOPBACK, PROXY_PORT)
+
+@pytest.fixture
+async def proxy_server():
+    _LOGGER.debug(">>> proxy_server")
+    server = ProxyServer(LOOPBACK, PROXY_PORT)
+    server_task = asyncio.create_task(server.run(), name="server")
+    yield server
+    await server.close()
+    await asyncio.sleep(0.25)
+    server_task.cancel()
+
 
 class TestSimpleRequests:
-    async def get(self, *args, **kargs):
-        kargs.update({ "proxy": f"http://{OWN_IP}:{PROXY_PORT}" })
-        _LOGGER.debug(f"kargs = {kargs}")
-        return await aiorequests.get(*args, **kargs)
-
-    @pytest.mark.asyncio
-    async def test_simple_request(self, httpserver: HTTPServer, event_loop):
-        setup_logging()
-        server = ProxyServer(OWN_IP, PROXY_PORT)
-        server_task = asyncio.create_task(server.run(), name="server")
-
+    async def test_simple_request(self, httpserver: HTTPServer, proxy_server, aiorequest):
         payload = { "message": "hello" }
         httpserver.expect_request("/hello").respond_with_json(payload)
 
-        # client_task = asyncio.create_task(self.get(httpserver.url_for("/hello")))
-        # status, response = await self.get(httpserver.url_for("/hello"))
-        response = await self.get(httpserver.url_for("/hello"))
-        # await asyncio.wait([server_task, client_task], return_when=asyncio.FIRST_COMPLETED)
-        # await client_task
-        # status, response = await asyncio.wait([client_task])
-
-        # status, response = client_task.result()
+        response = await aiorequest.get(httpserver.url_for("/hello"))
         assert response.status == 200
         assert await response.json() == payload
-
-        # Close server and allow it to exit
-        await server.close()
-        await asyncio.sleep(0.25)
